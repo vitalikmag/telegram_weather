0fe3cd41545da1cb7c0ca8c07142c8a37285c12a
Vitaly Magomedov <120472135+vitalikmag@users.noreply.github.com>
2023-04-14 18:41:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = '#############################'
-tg_bot_token = '######################'
+open_weather_token = '{YOUR_OPEN_WEATHER_API_TOKEN}'
+tg_bot_token = 'YOUR_TG_BOT_TOKEN'
